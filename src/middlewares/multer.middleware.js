4a5e4ebc0be1f52fd44d9599e4b2497f35a627b0
Adityaaa7 <adityajagtap0705@gmail.com>
2025-06-27 00:01:57 +0530

    changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@
 
 import multer from "multer"
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({       //we'll upload the file from user on local disk storage  
     destination: function (req, file, cb) {
       cb(null, "./public/temp")                  //(null is for error handling, path is for destination)
     },
